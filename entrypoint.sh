a556e70d3cb5439209b5d22bdf1958f98bb68797
Alessandro <alessandro@otimize-ti.com.br>
2020-07-28 14:40:57 -0300

    Added PARALLEL_FILES variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,14 @@ set -e
 echo "starting pgbadger $PGBADGER_VERSION ..."
 echo ""
 echo "--jobs $JOBS"
+echo "--Jobs $PARALLEL_FILES"
 echo "--average $AVERAGE"
 echo "--sample $SAMPLE"
 echo "--top $TOP"
 
 mkdir -p /usr/share/nginx/html/pgbadger
 
-pgbadger --incremental --quiet --jobs $JOBS --average $AVERAGE --sample $SAMPLE --top $TOP /data/*.log --outdir /usr/share/nginx/html/pgbadger
+pgbadger --incremental --quiet --jobs $JOBS --Jobs $PARALLEL_FILES --average $AVERAGE --sample $SAMPLE --top $TOP /data/*.log --outdir /usr/share/nginx/html/pgbadger
 
 echo "... end of run"
 exit 0
